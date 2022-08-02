02d129f105718f6d717504ec752a65ac14d9dcc4
Hideo Hattori <hhatto.jp@gmail.com>
2022-08-02 09:48:07 +0900

    use actix-web v4

--- src/web.rs
+++ src/web.rs
@@ -1,17 +1,18 @@
 use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
-fn index() -> impl Responder {
+#[get("/")]
+async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
 pub fn serve() {
     let addr = "127.0.0.1:9999";
-    let _s = HttpServer::new(|| App::new().wrap(Logger::default()).service(web::resource("/").to(index)))
+    let _s = HttpServer::new(|| App::new().wrap(Logger::default()).service(index))
         .bind(addr)
         .expect("fail bind")
         .shutdown_timeout(0)
-        .start();
+        .run();
 
     info!("listen to {}", addr);
 }
